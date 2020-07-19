1621a2864e3d1412ab2d472a67e00948b412f895
Liam Obrochta <lao1711@g.rit.edu>
2020-07-18 20:41:58 -0400

    Initial Commit

---
+++ Blowfish/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Blowfish
+//
+//  Created by Liam Obrochta on 7/18/20.
+//  Copyright © 2020 Liam Obrochta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
