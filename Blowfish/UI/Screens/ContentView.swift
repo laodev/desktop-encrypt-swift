5f3043ab686612142e79a242f370f48204fc42f0
Liam Obrochta <lao1711@g.rit.edu>
2020-07-19 23:31:44 -0400

    hstack used to position button nav and apply correct window dimensions

--- Blowfish/UI/Screens/ContentView.swift
+++ Blowfish/UI/Screens/ContentView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            ButtonNav()
+            VStack {
+                Text("test")
+            }
+        }
+        .frame(width: 1366, height: 768, alignment: .leading)
+    .background(Color(red: 74 / 255, green: 96 / 255, blue: 96 / 255))
     }
 }
 
