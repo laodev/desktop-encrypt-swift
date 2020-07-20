367597ca9b1136818eb5b0c3872fe72f40f01f2f
Liam Obrochta <lao1711@g.rit.edu>
2020-07-20 11:17:42 -0400

    initial commit for encrypt view

---
+++ Blowfish/UI/Screens/EncryptView.swift
@@ -0,0 +1,21 @@
+//
+//  EncryptView.swift
+//  Blowfish
+//
+//  Created by Liam Obrochta on 7/20/20.
+//  Copyright © 2020 Liam Obrochta. All rights reserved.
+//
+
+import SwiftUI
+
+struct EncryptView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EncryptView_Previews: PreviewProvider {
+    static var previews: some View {
+        EncryptView()
+    }
+}
