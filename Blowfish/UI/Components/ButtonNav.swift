b8dd94644d65629e612cb07629034e5090c00ab4
Liam Obrochta <lao1711@g.rit.edu>
2020-07-19 23:30:44 -0400

    vstacks used to properly position buttons

--- Blowfish/UI/Components/ButtonNav.swift
+++ Blowfish/UI/Components/ButtonNav.swift
@@ -10,8 +10,50 @@ import SwiftUI
 
 struct ButtonNav: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            VStack {
+                Button(action: onClick) {
+                    Image("marker")
+                    .renderingMode(.original)
+                }
+                .frame(width: 64, height: 50, alignment: .center)
+                Button(action: onClick) {
+                    Image("marker")
+                    .renderingMode(.original)
+                }
+                .frame(width: 64, height: 50, alignment: .center)
+                Button(action: onClick) {
+                    Image("marker")
+                    .renderingMode(.original)
+                }
+                .frame(width: 64, height: 50, alignment: .center)
+                Button(action: onClick) {
+                    Image("marker")
+                    .renderingMode(.original)
+                }
+                .frame(width: 64, height: 50, alignment: .center)
+            }
+                
+            .frame(maxWidth: 64)
+            
+            VStack {
+                Button(action: onClick) {
+                               Image("marker")
+                               .renderingMode(.original)
+                           }
+                .frame(width: 64, height: 50, alignment: .center)
+            }
+            .frame(maxWidth: 64, maxHeight: .infinity, alignment: .bottom)
+        }
+        .background(Color(red: 96 / 255, green: 96 / 255, blue: 96 / 255))
+        .frame(minWidth: 64, maxWidth: 64, minHeight: 768, maxHeight: .infinity)
+
     }
+}
+
+func onClick() {
+    // Insert code here to tear down your application
 }
 
 struct ButtonNav_Previews: PreviewProvider {
--- Blowfish/UI/Screens/ContentView.swift
+++ Blowfish/UI/Screens/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  Blowfish
 //
-//  Created by Liam Obrochta on 7/18/20.
+//  Created by Liam Obrochta on 7/19/20.
 //  Copyright © 2020 Liam Obrochta. All rights reserved.
 //
 
@@ -10,19 +10,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Button("Register", action: click)
-            Button("Register", action: click)
-        }
-        .frame(minWidth: 0, maxWidth: 64, minHeight: 0, maxHeight: .infinity)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
-    
 }
-
-func click() -> Void {
-    return
-}
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
